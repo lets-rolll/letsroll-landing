46c4aa67f3af69151c8bdf86cac538ad928848d3
archeda <90338869+archeda@users.noreply.github.com>
2023-02-09 20:10:49 +0300

    created landing layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// @ts-ignore
+import InfiniteCarousel from 'react-leaf-carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <section>
+      <div className="py-10 h-full flex flex-col justify-between text-center lg:text-4xl">
+        <p>СЕРВИС ПОМОЩИ ПУТЕШЕСТВЕННИКАМ И РЕЛОКАНТАМ</p>
+        <InfiniteCarousel className="h-full">
+          <div className="slide flex items-center justify-center text-[144px] font-extrabold">
+            <p>LET’S ROLL</p>
+          </div>
+          <div className="slide flex flex-row items-center justify-center text-left text-[64px] leading-[64px]">
+            <p>Помогаем построить<br />маршрут, подобрать<br />жилье и влиться<br />в комьюнити</p>
+            <button className="ml-[95px] text-2xl mt-[150px] btn-green">Попробовать</button>
+          </div>
+        </InfiniteCarousel>
+        <p>ПУТЕШЕСТВУЙ ПРОСТО</p>
+      </div>
+    </section>
+    <section className="scroll-horizontal">
+      <div className="sidebar">
+        
+      </div>
+      <div className="slide-container">
+        <div className="slide bg-green-300">slide1</div>
+        <div className="slide bg-red-300">slide2</div>
+        <div className="slide bg-blue-300">slide3</div>
+        <div className="slide bg-orange-300">slide4</div>
+        <div className="slide bg-yellow-300">slide5</div>
+      </div>
+    </section>
+
+    <section className="bg-blue-300">
+      block 3
+    </section>
+
+    <section className="bg-green-300">
+      block 4
+    </section>
+  </div>
   );
 }
 
